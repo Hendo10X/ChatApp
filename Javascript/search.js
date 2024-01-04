b89100efef98f2c312d2f667c0e8c9a2b8665464
Henderson Dike-Benard <83480908+Hendo10X@users.noreply.github.com>
2024-01-04 20:07:44 +0100

    Made the search button in the login toggle and Changed the display

--- Javascript/search.js
+++ Javascript/search.js
@@ -2,5 +2,7 @@ const SearchBar = document.querySelector(".users .search input"),
 SearchBtn = document.querySelector(".users .search button");
 
 SearchBtn.onclick = ()=> {
-    SearchBar.classList("active");
+    SearchBar.classList.toggle("active");
+    SearchBar.focus();
+    SearchBtn.classList.toggle("active");
 }
